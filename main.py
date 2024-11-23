599f41f3b19f99bad18c1e72cc28600b11a4708a
speedruneer <101725579+speedruneer@users.noreply.github.com>
2024-11-23 22:06:47 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ def get_github_file_content(url):
         return response.text  # Returns the raw content as a string
     else:
         raise Exception(f"Error fetching the file: {response.status_code}")
+github_raw_url = "https://raw.githubusercontent.com/speedruneer/Testing/refs/heads/main/main.py"
+github_raw_url_mvex = "https://raw.githubusercontent.com/speedruneer/Testing/refs/heads/main/mv_ex.py"
